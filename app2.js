9ed8be8eab04ab892f196cf51b4ba86a31d8814f
Mehboob-Ali-Romeo <mehboobalilangove786@gmail.com>
2025-11-29 08:09:25 -0800

    completed

---
+++ app2.js
@@ -0,0 +1,208 @@
+// let age = prompt("Enter your age:");
+
+// if (age >= 18) {
+//     alert("You are an adult.");
+// } else {
+//     alert("You are a minor.");
+// }
+
+
+
+// let num = prompt("Enter a number:");
+
+// if (num > 0) {
+//     console.log("Positive number");
+// } else {
+//     console.log("Not positive");
+// }
+
+
+
+// let n = prompt("Enter a number:");
+
+// if (n % 2 === 0) {
+//     alert("Even number");
+// } else {
+//     alert("Odd number");
+// }
+
+
+
+
+// let user = prompt("Enter username:");
+
+// if (user === "admin") {
+//     alert("Welcome, admin!");
+// } else {
+//     alert("Access denied.");
+// }
+
+
+
+// let a = prompt("Enter first number:");
+// let b = prompt("Enter second number:");
+
+// if (a == b) {
+//     console.log("Both numbers are the same");
+// } else {
+//     console.log("The numbers are different");
+// }
+
+
+
+
+// let x = prompt("Enter first number:");
+// let y = prompt("Enter second number:");
+
+// if (x > y) {
+//     alert("First is larger");
+// } else {
+//     alert("Second is larger");
+// }
+
+
+
+// let num7 = prompt("Enter a number:");
+
+// if (num7 > 100) {
+//     alert("Too big!");
+// } else {
+//     alert("Within range");
+// }
+
+
+
+// let score = prompt("Enter your score:");
+
+// if (score >= 50) {
+//     console.log("You passed!");
+// } else {
+//     console.log("You failed!");
+// }
+
+
+
+
+// let temp = prompt("Enter temperature:");
+
+// if (temp > 30) {
+//     alert("It's hot!");
+// } else {
+//     alert("It's not hot.");
+// }
+
+
+
+// let z = prompt("Enter a number:");
+
+// if (z == 0) {
+//     alert("Zero");
+// } else {
+//     alert("Not zero");
+// }
+
+
+
+// let age11 = prompt("Enter your age:");
+
+// if (age11 >= 60) {
+//     console.log("You are a senior citizen.");
+// } else {
+//     console.log("You are not a senior.");
+// }
+
+
+
+// let name12 = prompt("Enter your name:");
+
+// if (name12 === "Alice") {
+//     alert("Hi, Alice!");
+// } else {
+//     alert("You're not Alice!");
+// }
+
+
+
+
+// let pass = prompt("Enter password:");
+
+// if (pass === "12345") {
+//     console.log("Correct password");
+// } else {
+//     console.log("Wrong password");
+// }
+
+
+
+// let hour = prompt("Enter current hour (0–23):");
+
+// if (hour < 12) {
+//     alert("Good morning!");
+// } else {
+//     alert("Good afternoon!");
+// }
+
+
+
+// let num15 = prompt("Enter a number:");
+
+// if (num15 % 5 === 0) {
+//     console.log("Divisible by 5");
+// } else {
+//     console.log("Not divisible by 5");
+// }
+
+
+
+// let game = prompt("Do you want to play a game? (yes/no)");
+
+// if (game === "yes") {
+//     alert("Let's play!");
+// } else {
+//     alert("Maybe next time!");
+// }
+
+
+
+
+// let color = prompt("Enter a color:");
+
+// if (color === "blue") {
+//     console.log("Cool choice!");
+// } else {
+//     console.log("Interesting color!");
+// }
+
+
+
+// let day = prompt("Enter day of the week:");
+
+// if (day === "Sunday") {
+//     alert("Weekend!");
+// } else {
+//     alert("Weekday");
+// }
+
+
+
+// let fruit = prompt("Enter a fruit:");
+
+// if (fruit === "banana" || fruit === "apple") {
+//     alert("We have that!");
+// } else {
+//     alert("Sorry, out of stock.");
+// }
+
+
+
+// let start = prompt('Type "start" to begin:');
+
+// if (start === "start") {
+//     alert("Game started!");
+// } else {
+//     alert("Waiting for input...");
+// }
+
+
+
+// Completed 
